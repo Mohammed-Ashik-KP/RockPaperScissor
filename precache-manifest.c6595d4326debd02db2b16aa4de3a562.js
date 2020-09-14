e3565a19dec5e4f94d070464cb7001a268dc104d
Aashiq-Otp <Aashiqotp.as@gmail.com>
2020-09-14 15:14:16 +0530

    Updates

---
+++ precache-manifest.c6595d4326debd02db2b16aa4de3a562.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e8a5b411080a08897095978de69aa1",
+    "url": "/RockPaperScissor/index.html"
+  },
+  {
+    "revision": "eb06482417c405a72053",
+    "url": "/RockPaperScissor/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "1130072031b3088ff3d5",
+    "url": "/RockPaperScissor/static/css/main.ebcd2fba.chunk.css"
+  },
+  {
+    "revision": "eb06482417c405a72053",
+    "url": "/RockPaperScissor/static/js/2.5ab029e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RockPaperScissor/static/js/2.5ab029e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1130072031b3088ff3d5",
+    "url": "/RockPaperScissor/static/js/main.ed47148f.chunk.js"
+  },
+  {
+    "revision": "f487271fa12cfc8b2fdf",
+    "url": "/RockPaperScissor/static/js/runtime-main.8e3e5489.js"
+  }
+]);
